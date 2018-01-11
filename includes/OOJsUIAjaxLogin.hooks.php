29497dcf3f7f226f9380702caa930f32b4bf0681
Umherirrender <umherirrender_de.wp@web.de>
2018-01-11 20:42:04 +0100

    Improve some parameter docs
    Change-Id: I7702c3019bd003e6ba79266c217c04961e007767

--- includes/OOJsUIAjaxLogin.hooks.php
+++ includes/OOJsUIAjaxLogin.hooks.php
@@ -4,8 +4,8 @@ class OOJsUIAjaxLoginHooks {
 	/**
 	 * BeforePageDisplay hook handler. Add the required module for this extension.
 	 *
-	 * @param OutputPage $out
-	 * @param Skin $sk
+	 * @param OutputPage &$out
+	 * @param Skin &$sk
 	 */
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$sk ) {
 		if ( !$out->getUser()->isLoggedIn() ) {
